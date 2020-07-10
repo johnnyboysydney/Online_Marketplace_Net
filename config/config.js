e22889b1cf095350f88295fa921effeec937eeff
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-10 22:01:32 +1000

    adding folder structure

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",  //The secret key to be used to sign JWT
+    mongoUri: process.env.MONGODB_URI ||                     //The location of the MongoDB database
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject'
+    }
+    export default config
