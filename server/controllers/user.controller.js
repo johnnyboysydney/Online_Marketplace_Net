cba9bd63ddad48c74b538b33b64cd1e3d68b21af
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-12 09:53:59 +1000

    adding create, list and userby Id functions

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,11 +3,46 @@ import User from '../models/user.model'
 import extend from 'lodash/extend'
 import errorHandler from './error.controller'
 
-const create = (req, res, next) => { … }
-const list = (req, res) => { … }
-const userByID = (req, res, next, id) => { … }
+const create = (req, res, next) => {
+    const user = new User(req.body)
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler.getErrorMessage(err)
+        })
+      }
+      res.status(200).json({
+        message: "Successfully signed up!"
+      })
+    })
+}
+
+const list = (req, res) => {
+    User.find((err, users) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler.getErrorMessage(err)
+        })
+      }
+      res.json(users)
+    }).select('name email updated created')
+}
+
+const userByID = (req, res, next, id) => {
+    User.findById(id).exec((err, user) => {
+      if (err || !user)
+        return res.status('400').json({
+          error: "User not found"
+        })
+      req.profile = user
+      next()
+    })
+  }
+
 const read = (req, res) => { … }
+
 const update = (req, res, next) => { … }
+
 const remove = (req, res, next) => { … }
 
 export default { create, userByID, read, list, remove, update }
