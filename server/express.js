9b8000cfdc2ea7fe12cd963e2841208a8523f6b9
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-11 18:15:41 +1000

    introducing modifications

--- server/express.js
+++ server/express.js
@@ -1,43 +1,18 @@
 // Dependencies
 import express from 'express'
-import path from 'path'
-import cookieParser from 'cookieParser'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
-import Template from './../template'
-import userRoutes from './routes/user.routes'
-import authRoutes from './routes/auth.routes'
 
-// modules for server side rendering
-
-
-// end
-const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
-// parse body params and attache them to req.body
 app.use(express.json())
-app.use(express.urlencoded({ extended: true })) 
-app.use(cookieParser()) //Cookie parsing middleware to parse and set cookies in request objects.
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(compress())
+app.use(helmet())
+app.use(cors())
 
-// secure apps by setting various HTTP headers
-
-
-// enable CORS - Cross Origin Resource Sharing
-
-
-// mount routes
-app.use('/', userRoutes)
-app.use('/', authRoutes)
-
-// Catch unauthorised errors
-app.use((err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).json({"error" : err.name + ": " + err.message})
-    }
-  })
-  
-  export default app
-  
+export default app
---
+++ server/server.js
@@ -0,0 +1,12 @@
+
+
+// Connection URL
+import config from './../config/config'
+import app from './express'
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', config.port)
+})
