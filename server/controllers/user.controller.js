7a85bfe9b42704dbadfe56537d0f8339095d23d4
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-12 10:03:48 +1000

    converting functions using async and await and adding try/catch

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,41 +3,46 @@ import User from '../models/user.model'
 import extend from 'lodash/extend'
 import errorHandler from './error.controller'
 
-const create = (req, res, next) => {
+const create = async (req, res) => {
     const user = new User(req.body)
-    user.save((err, result) => {
-      if (err) {
-        return res.status(400).json({
-          error: errorHandler.getErrorMessage(err)
+    try {
+        await user.save()
+        return res.status(200).json({
+            message: "Successfully signed up!"
         })
-      }
-      res.status(200).json({
-        message: "Successfully signed up!"
-      })
-    })
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
 }
 
-const list = (req, res) => {
-    User.find((err, users) => {
-      if (err) {
+const list = async (req, res) => {
+    try {
+        let users = await User.find().select('name email updated created')
+        res.json(users)
+    } catch (err) {
         return res.status(400).json({
-          error: errorHandler.getErrorMessage(err)
+            error: errorHandler.getErrorMessage(err)
         })
-      }
-      res.json(users)
-    }).select('name email updated created')
+    }
 }
 
-const userByID = (req, res, next, id) => {
-    User.findById(id).exec((err, user) => {
-      if (err || !user)
+const userByID = async (req, res, next, id) => {
+    try {
+        let user = await User.findById(id)
+        if (!user)
         return res.status('400').json({
-          error: "User not found"
+            error: "User not found"
         })
-      req.profile = user
-      next()
-    })
-  }
+        req.profile = user
+        next()
+    } catch (err) {
+        return res.status('400').json({
+            error: "Could not retrieve user"
+        })
+    }
+}
 
 const read = (req, res) => { … }
 
