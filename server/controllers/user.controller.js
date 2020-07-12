eb89399700510be840b3cfc1fa973b03a85824b2
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-12 10:09:07 +1000

    adding update function

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -17,6 +17,7 @@ const create = async (req, res) => {
     }
 }
 
+// Load user and append to the req
 const list = async (req, res) => {
     try {
         let users = await User.find().select('name email updated created')
@@ -44,9 +45,27 @@ const userByID = async (req, res, next, id) => {
     }
 }
 
-const read = (req, res) => { … }
+const read = (req, res) => {
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+    return res.json(req.profile)
+}
 
-const update = (req, res, next) => { … }
+// updating a single user
+const update = async (req, res) => {
+    try {
+        let user = req.profile
+        user = extended(user, req.body)
+        await user.save()
+        user.hashed_password = undefined
+        user.salt = undefined
+        res.json(user)
+    } catch {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
 
 const remove = (req, res, next) => { … }
 
