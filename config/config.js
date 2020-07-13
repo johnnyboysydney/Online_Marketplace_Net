adcb2147466d2287e64b165ce4fcf070cf71d3d0
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-13 18:49:35 +1000

    Added dotenv to access process.env

--- config/config.js
+++ config/config.js
@@ -1,12 +1,21 @@
+const dotenv = require("dotenv");
+
+// get config vars
+dotenv.config();
+
+// access config var
+process.env.TOKEN_SECRET;
+
 const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",  //The secret key to be used to sign JWT
-    mongoUri: process.env.MONGODB_URI ||                     //The location of the MongoDB database
-    process.env.MONGO_HOST ||
+    //The secret key to be used to sign JWT
+    jwtSecret: process.env.TOKEN_SECRET || TOKEN_SECRET,   
+     //The location of the MongoDB database
+    mongoUri: process.env.MONGODB_URI ||                                      
+    process.env.MONGO_HOST ||                  
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
+    (process.env.MONGO_PORT || '27017') + '/mernproject'
 }
 
 export default config
