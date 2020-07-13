6058a8f97b776bad9f0bd8c2dc2f92d06eb98d7a
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-13 18:52:43 +1000

    commenting out unnecesary imports

--- server/express.js
+++ server/express.js
@@ -3,20 +3,43 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
+import path from 'path'
 import helmet from 'helmet'
 import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
+// modules for server side rendering
+//import React from 'react'
+//import ReactDOMServer from 'react-dom/server'
+//import MainRouter from './../client/MainRouter'
+//import StaticRouter from 'react-router-dom/StaticRouter'
+//import { SheetsRegistry } from 'react-jss/lib/jss'
+//import JssProvider from 'react-jss/lib/JssProvider'
+//import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from 'material-ui/styles'
+//import { indigo, pink } from 'material-ui/colors'
+//end
+
+//comment out before building for production
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
+//comment out before building for production
+devBundle.compile(app)
+
+// parse body params and attach them to req.body
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
+// secure apps by setting various HTTP headers
 app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 // mount routes
 app.use('/', userRoutes)
