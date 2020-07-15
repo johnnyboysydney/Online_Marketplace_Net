3fd9ec36035fca8cb653ab8bea6ae6a54efc88b5
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-15 19:37:53 +1000

    adding shop and product routes

---
+++ server/routes/product.routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import productCtrl from '../controllers/product.controller'
+import authCtrl from '../controllers/auth.controller'
+import shopCtrl from '../controllers/shop.controller'
+
+const router = express.Router()
+
+export default router
---
+++ server/routes/shop.routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import userCtrl from '../controllers/user.controller'
+import authCtrl from '../controllers/auth.controller'
+import shopCtrl from '../controllers/shop.controller'
+
+const router = express.Router()
+
+export default router
