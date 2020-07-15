cb00c75972837ab3c78f028a56847555a69a8108
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-15 19:34:50 +1000

    added markup ReactDomserver

--- server/express.js
+++ server/express.js
@@ -1,23 +1,27 @@
-// Dependencies
 import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
-import path from 'path'
 import helmet from 'helmet'
 import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
+import shopRoutes from './routes/shop.routes'
+import productRoutes from './routes/product.routes'
+import orderRoutes from './routes/order.routes'
 
 // modules for server side rendering
-//import React from 'react'
-//import ReactDOMServer from 'react-dom/server'
-//import MainRouter from './../client/MainRouter'
-//import StaticRouter from 'react-router-dom/StaticRouter'
-//import { SheetsRegistry } from 'react-jss/lib/jss'
-//import JssProvider from 'react-jss/lib/JssProvider'
-//import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from 'material-ui/styles'
-//import { indigo, pink } from 'material-ui/colors'
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import StaticRouter from 'react-router-dom/StaticRouter'
+
+import { SheetsRegistry } from 'react-jss/lib/jss'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from 'material-ui/styles'
+import { blueGrey, lightGreen } from 'material-ui/colors'
 //end
 
 //comment out before building for production
@@ -29,9 +33,9 @@ const app = express()
 //comment out before building for production
 devBundle.compile(app)
 
-// parse body params and attach them to req.body
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 // secure apps by setting various HTTP headers
@@ -44,17 +48,57 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 // mount routes
 app.use('/', userRoutes)
 app.use('/', authRoutes)
+app.use('/', shopRoutes)
+app.use('/', productRoutes)
+app.use('/', orderRoutes)
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
-    })
-
-app.use((err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({"error" : err.name + ": " + err.message })
-    } else if (err) {
-        res.status(400).json({"error": err.name + ": " + err.message })
+app.get('*', (req, res) => {
+   const sheetsRegistry = new SheetsRegistry()
+   const theme = createMuiTheme({
+     palette: {
+       primary: {
+       light: '#8eacbb',
+       main: '#607d8b',
+       dark: '#34515e',
+       contrastText: '#fff',
+     },
+     secondary: {
+       light: '#e7ff8c',
+       main: '#b2ff59',
+       dark: '#7ecb20',
+       contrastText: '#000',
+     },
+       openTitle: blueGrey['400'],
+       protectedTitle: lightGreen['400'],
+       type: 'light'
+     }
+   })
+   const generateClassName = createGenerateClassName()
+   const context = {}
+   const markup = ReactDOMServer.renderToString(
+      <StaticRouter location={req.url} context={context}>
+         <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+            <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+              <MainRouter/>
+            </MuiThemeProvider>
+         </JssProvider>
+      </StaticRouter>
+     )
+    if (context.url) {
+      return res.redirect(303, context.url)
     }
+    const css = sheetsRegistry.toString()
+    res.status(200).send(Template({
+      markup: markup,
+      css: css
+    }))
 })
 
-export default app;
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }
+})
+
+export default app
