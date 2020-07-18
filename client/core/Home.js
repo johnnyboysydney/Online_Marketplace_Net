2033c3ecac7fa67eca8ad754c49c1c8c9b14d2b2
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-18 22:04:05 +1000

    adding styles

--- client/core/Home.js
+++ client/core/Home.js
@@ -3,9 +3,22 @@ import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
 import Card, {CardContent, CardMedia} from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
-import seashellImg from './../assets/images/seashell.jpg'
+import marketplaceImg from './../assets/images/marketplace.jpg'
 
-
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+})
 class Home extends Component {
   render() {
     const {classes} = this.props
