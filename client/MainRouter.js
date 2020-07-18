ae4ee2dfd2303dd9c01f6d805f96cbe811cb5931
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-18 22:34:20 +1000

    adding imports for all routes

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './core/Home'
+import Users from './user/Users'
+import Signup from './user/Signup'
+import Signin from './auth/Signin'
+import EditProfile from './user/EditProfile'
+import Profile from './user/Profile'
+import PrivateRoute from './auth/PrivateRoute'
+import Menu from './core/Menu'
 
 
 class MainRouter extends Component {
