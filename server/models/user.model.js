be8ba1f88c5c3e52a74dfdbac6eb5bf773244a65
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-18 19:04:23 +1000

    refactored modeles/users

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,43 +1,30 @@
 import mongoose from 'mongoose'
 import crypto from 'crypto'
-
 const UserSchema = new mongoose.Schema({
-    // The name field is a required field of the String type
-    name: {
-      type: String,
-      trim: true,
-      required: 'Name is required'
-    },
-    // The email field is a required field of the String type
-    email: {
-      type: String,
-      trim: true,
-      unique: 'Email already exists',
-      match: [/.+\@.+\..+/, 'Please fill a valid email address'],
-      required: 'Email is required'
-    },
-    // The hashed_password and salt fields represent the encrypted user password
-    hashed_password: {
-      type: String,
-      required: "Password is required"
-    },
-    salt: String,
-    updated: Date,
-    // The created and updated fields are Date values.
-    created: {
-      type: Date,
-      default: Date.now
-    },
-    // The seller type
-    seller: {
-      type: Boolean,
-      default: false
-    },
-    stripe_seller: {},
-    stripe_customer: {}
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 'Email already exists',
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email is required'
+  },
+  hashed_password: {
+    type: String,
+    required: "Password is required"
+  },
+  salt: String,
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 })
 
-// The password string that's provided by the user it is handled as a virtual field.
 UserSchema
   .virtual('password')
   .set(function(password) {
@@ -49,7 +36,6 @@ UserSchema
     return this._password
   })
 
-// Password field validation
 UserSchema.path('hashed_password').validate(function(v) {
   if (this._password && this._password.length < 6) {
     this.invalidate('password', 'Password must be at least 6 characters.')
@@ -59,7 +45,6 @@ UserSchema.path('hashed_password').validate(function(v) {
   }
 }, null)
 
-// The encryption logic and salt generation logic,
 UserSchema.methods = {
   authenticate: function(plainText) {
     return this.encryptPassword(plainText) === this.hashed_password
