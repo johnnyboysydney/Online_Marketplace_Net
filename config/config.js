31b78d68a4a07ac7c3dedcdde32ea2ea0badd03d
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-18 22:46:19 +1000

    adding stripe key

--- config/config.js
+++ config/config.js
@@ -6,7 +6,10 @@ const config = {
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
+    '/mernproject',
+    stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
+    stripe_test_secret_key: 'sk_test_51H6E6yEV0d7rwUXNpphZoFt42839vrqg8xgsdAnQcAuSrlxUS1GPRMrwP0gvdaqhgS9PpeESEwZtEorIo5cvaG95003wMGphVr',
+    stripe_test_api_key: 'pk_test_51H6E6yEV0d7rwUXNgJU9onIdFyA4BksuQr7yFpFPAUryF7mVd0naFCx1nggH7D7OAZXgCywmBSooD1YOmTZCf1Ir00m1HwJ988'
 }
 
 export default config
