d9df7fe01c746f9ddd95c434155a2292d7a44b34
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-18 22:33:15 +1000

    addig classmainrouter and render menu switch

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,5 +1,25 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
+import Home from './core/Home'
 
+
+class MainRouter extends Component {
+  // Removes the server-side injected CSS when React component mounts
+  componentDidMount() {
+    const jssStyles = document.getElementById('jss-server-side')
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }
+
+  render() {
+    return (<div>
+      <Menu/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+      </div>)
+  }  
+}
 
 export default MainRouter
