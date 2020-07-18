b81f92219705dc22bcdb535a477387359547dd46
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-18 19:02:54 +1000

    fixed errors

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import authCtrl from '../controllers/auth.controller'
-import router from '../routes/user.routes'
+
+const router = express.Router()
 
 router.route('/auth/signin')
-    .post (authCtrl.signin)
-    
+  .post(authCtrl.signin)
 router.route('/auth/signout')
-    .get (authCtrl.signout)
+  .get(authCtrl.signout)
 
-export default router;
+export default router
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,7 @@
-// Depeendencies & variables
 import express from 'express'
 import userCtrl from '../controllers/user.controller'
 import authCtrl from '../controllers/auth.controller'
+
 
 const router = express.Router()
 
