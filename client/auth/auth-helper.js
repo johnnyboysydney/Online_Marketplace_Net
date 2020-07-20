44f85ace49ab3aad4466fd95d143bbe4a31ad019
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 13:23:19 +1000

    added another if and else to isAuthenticated function

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -4,6 +4,10 @@ const auth = {
     isAuthenticated() {
         if (typeof window == "undefined")
             return false
+        if (sessionStorage.getItem('jwt'))
+            return JSON.parse(sessionStorage.getItem('jwt'))
+        else
+            return false
     },
     authenticate(jwt, cb) {
       if (typeof window !== "undefined")
@@ -14,6 +18,9 @@ const auth = {
         if (typeof window !== "undefined")
           sessionStorage.removeItem('jwt')
         cb()
+        signout().then((data) => {
+            document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+        })
     }
 }
 
