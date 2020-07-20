bd9463610e7599e4f82a236987253d8a1352ca69
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 12:59:42 +1000

    Adding edit profile

---
+++ client/user/EditProfile.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import Card, {CardActions, CardContent} from 'material-ui/Card'
+import Button from 'material-ui/Button'
+import TextField from 'material-ui/TextField'
+
+
+const styles = theme => ({ 
+
+})
+
+class EditProfile extends Component { 
+    
+}
+
+
+
+EditProfile.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+  
+export default withStyles(styles)(EditProfile)
+  
+
+
+
+
+
