ad43f424a3c0a63d0814a7e4197b77e96b726839
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 20:04:26 +1000

    adding the list to home

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,39 +1,55 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
-import Card, {CardContent, CardMedia} from 'material-ui/Card'
-import Typography from 'material-ui/Typography'
-import marketplaceImg from './../assets/images/marketplace.jpg'
+import Grid from 'material-ui/Grid'
+import Suggestions from './../product/Suggestions'
+import {listLatest, listCategories} from './../product/api-product.js'
+import Search from './../product/Search'
+import Categories from './../product/Categories'
 
 const styles = theme => ({
-  card: {
-    maxWidth: 600,
-    margin: 'auto',
-    marginTop: theme.spacing.unit * 5
-  },
-  title: {
-    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
-    color: theme.palette.text.secondary
-  },
-  media: {
-    minHeight: 330
+  root: {
+    flexGrow: 1,
+    margin: 30,
   }
 })
+
 class Home extends Component {
+  state={
+    suggestionTitle: "Latest Products",
+    suggestions: [],
+    categories: []
+  }
+  componentDidMount = () => {
+    listLatest().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        this.setState({suggestions: data})
+      }
+    })
+    listCategories().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        this.setState({categories: data})
+      }
+    })
+  }
   render() {
     const {classes} = this.props
     return (
-        <Card className={classes.card}>
-          <Typography type="headline" component="h2" className={classes.title}>
-            Home Page
-          </Typography>
-          <CardMedia className={classes.media} image={marketplaceImg} title="Marketplace"/>
-          <CardContent>
-            <Typography type="body1" component="p">
-              Welcome to the Online Market Place.
-            </Typography>
-          </CardContent>
-        </Card>
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={8} sm={8}>
+            <Search categories={this.state.categories}/>
+            <Categories categories={this.state.categories}/>
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <Suggestions products={this.state.suggestions} title={this.state.suggestionTitle}/>
+          </Grid>
+        </Grid>
+      </div>
     )
   }
 }
