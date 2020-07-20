8d9d0c87ebeb5659369202a628861ca9935735a5
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 19:58:16 +1000

    adding routes for orders

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import orderCtrl from '../controllers/order.controller'
-import productCtrl from '../controllers/product.controller'
-import authCtrl from '../controllers/auth.controller'
-import shopCtrl from '../controllers/shop.controller'
-import userCtrl from '../controllers/user.controller'
+//import orderCtrl from '../controllers/order.controller'
+//import productCtrl from '../controllers/product.controller'
+//import authCtrl from '../controllers/auth.controller'
+//import shopCtrl from '../controllers/shop.controller'
+//import userCtrl from '../controllers/user.controller'
 
 const router = express.Router()
 
