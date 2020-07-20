b925320e97be704c16feca08993666f09e45ae1b
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 15:44:54 +1000

    refactoring code

--- client/auth/api-auth.js
+++ client/auth/api-auth.js
@@ -1,24 +1,28 @@
 const signin = (user) => {
-    return fetch('/auth/signin', {
+    return fetch('/auth/signin/', {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
         },
         credentials: 'include',
         body: JSON.stringify(user)
-    })
-    .then((response)=> {
+      })
+      .then((response) => {
+        return response.json()
+      }).catch((err) => console.log(err))
+  }
+  
+  const signout = () => {
+    return fetch('/auth/signout/', {
+      method: 'GET',
+    }).then(response => {
         return response.json()
     }).catch((err) => console.log(err))
-}
-
-const signout =() => {
-    return fetch('/auth/signout', {
-        method: 'GET',
-    }).then(reponse => {
-        return response.json()
-    }).catch((err) => console.log(err))
-}
-
-export {signin, signout}
+  }
+  
+  export {
+    signin,
+    signout
+  }
+  
