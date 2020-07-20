755883851b98880bac8a74e57e348644b4d109ea
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 19:59:04 +1000

    adding routes for shops

--- server/routes/shop.routes.js
+++ server/routes/shop.routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import userCtrl from '../controllers/user.controller'
-import authCtrl from '../controllers/auth.controller'
-import shopCtrl from '../controllers/shop.controller'
+//import userCtrl from '../controllers/user.controller'
+//import authCtrl from '../controllers/auth.controller'
+//import shopCtrl from '../controllers/shop.controller'
 
 const router = express.Router()
 
