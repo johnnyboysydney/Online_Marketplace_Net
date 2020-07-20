05ac13e7c37ec51296f5b5bc8673fecb6e0bf57f
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 12:59:17 +1000

    adding deleting

---
+++ client/user/DeleteUser.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import IconButton from 'material-ui/IconButton'
+import Button from 'material-ui/Button'
+import DeleteIcon from 'material-ui-icons/Delete'
+
+class DeleteUser extends Component {
+
+}
+
+DeleteUser.propTypes = {
+    userId: PropTypes.string.isRequired
+}
+export default DeleteUser
