b5b5e766f13fd618dbfe97fb52f2a952412881a1
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 12:51:27 +1000

    adding the CRUD api

---
+++ client/user/api-user.js
@@ -0,0 +1,27 @@
+const create = (user) => {
+
+}
+
+const list = () => {
+
+}
+
+const read = (params, credentials) => {
+
+}
+
+const update = (params, credentials, user) => {
+
+}
+
+const remove = (params, credentials) => {
+
+}
+
+export {
+    create,
+    list,
+    read,
+    update,
+    remove
+  }
