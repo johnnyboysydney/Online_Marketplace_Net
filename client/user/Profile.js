165944435d2c7a0cfdce116022b378f673f9d06a
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 12:48:04 +1000

    adding Profile

---
+++ client/user/Profile.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from 'material-ui/styles'
+import Paper from 'material-ui/Paper'
+
+
+const styles = theme => ({ 
+
+})
+
+class Profile extends Component {
+
+}
+
+Profile.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+  
+export default withStyles(styles)(Profile)
