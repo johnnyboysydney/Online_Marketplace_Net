8193e6d384089b241fdaec1d9a4095a7c75e4112
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-20 12:46:32 +1000

    adding signup

---
+++ client/user/Signup.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Card, {CardActions, CardContent} from 'material-ui/Card'
+import Button from 'material-ui/Button'
+
+const styles = theme => ({ 
+
+})
+
+Signup.propTypes = {
+    classes: PropTypes.object.isRequired
+  }
+  
+  export default withStyles(styles)(Signup)
+  
