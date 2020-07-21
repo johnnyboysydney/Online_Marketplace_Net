65ad2a68ff398861a037eeab21607f0a5237c648
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 11:43:52 +1000

    adding react components

--- client/shop/NewShop.js
+++ client/shop/NewShop.js
@@ -1,8 +1,72 @@
 import React, {Component} from 'react'
 import Card, {CardActions, CardContent} from 'material-ui/Card'
 import Button from 'material-ui/Button'
+import FileUpload from 'material-ui-icons/FileUpload'
+import auth from './../auth/auth-helper'
+import TextField from 'material-ui/TextField'
+import Typography from 'material-ui/Typography'
+import Icon from 'material-ui/Icon'
+import PropTypes from 'prop-types'
+import {withStyles} from 'material-ui/styles'
+import {create} from './api-shop.js'
+import {Link, Redirect} from 'react-router-dom'
 
-class NewShop extends Component {}
+const styles = theme => ({})
+
+class NewShop extends Component {
+    state = {
+        name: '',
+        description: '',
+        image: '',
+        redirect: false,
+        error: ''
+    }
+    componentDidMount = () => {
+        this.shopData = new FormData()
+    }
+    handleChange = name => event => {
+        const value = name === 'image'
+          ? event.target.files[0]
+          : event.target.value
+        this.shopData.set(name, value)
+        this.setState({ [name]: value })
+    }
+    clickSubmit = () => {
+        const jwt = auth.isAuthenticated()
+        create({
+          userId: jwt.user._id
+        }, {
+          t: jwt.token
+        }, this.shopData).then((data) => {
+          if (data.error) {
+            this.setState({error: data.error})
+          } else {
+            this.setState({error: '', redirect: true})
+          }
+        })
+    }
+    render() {
+        if (this.state.redirect) {
+          return (<Redirect to={'/seller/shops'}/>)
+        }
+        const {classes} = this.props
+        return (
+        <div>
+            <Card>
+                <CardContent>
+                    <Typography type="headline" component="h2" className={classes.title}>
+                        New Shop
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>Submit</Button>
+                    <Link to='/seller/shops' className={classes.submit}><Button variant="raised">Cancel</Button></Link>
+                </CardActions>
+            </Card>
+        </div>
+        )
+    }
+}
 
 NewShop.propTypes = {
     classes: PropTypes.object.isRequired
