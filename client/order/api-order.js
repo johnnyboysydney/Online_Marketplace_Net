213807a3a0c2ac6e3e392be802f13ad2cee3addc
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 14:12:14 +1000

    adding api-orders initial code

---
+++ client/order/api-order.js
@@ -0,0 +1,19 @@
+const create = () => {}
+const listByShop = () => {}
+const update = () => {}
+const cancelProduct = () => {}
+const processCharge = () => {}
+const getStatusValues = () => {}
+const listByUser = () => {}
+const read = () => {}
+
+export {
+    create,
+    listByShop,
+    update,
+    cancelProduct,
+    processCharge,
+    getStatusValues,
+    listByUser,
+    read
+  }
