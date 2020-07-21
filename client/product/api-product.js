37898008cab56f10acaf473848dfbd728cce795c
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 12:14:54 +1000

    adding initial code

---
+++ client/product/api-product.js
@@ -0,0 +1,23 @@
+import queryString from 'query-string'
+
+const create = () => {}
+const read = () => {}
+const update = () => {}
+const remove = () => {}
+const listByShop = () => {}
+const listLatest = () => {}
+const listRelated = () => {}
+const listCategories = () => {}
+const list = () => {}
+
+export {
+    create,
+    read,
+    update,
+    remove,
+    listByShop,
+    listLatest,
+    listRelated,
+    listCategories,
+    list
+  }
