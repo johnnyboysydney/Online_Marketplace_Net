0b8c03a547cd40c304e1074214985398dd45d46d
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 11:24:51 +1000

    adding edit shop and initial code

---
+++ client/shop/EditShop.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import auth from './../auth/auth-helper'
+import Card, {CardActions, CardContent, CardMedia} from 'material-ui/Card'
+
+const styles = theme => ({})
+
+class EditShop extends Component {}
+
+EditShop.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+  
+export default withStyles(styles)(EditShop)
