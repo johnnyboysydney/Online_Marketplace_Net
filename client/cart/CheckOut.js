c594f0ee6aaf4dd897ba910ce43eb3919db6a765
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 11:30:24 +1000

    adding react components

--- client/cart/CheckOut.js
+++ client/cart/CheckOut.js
@@ -31,16 +31,33 @@ class Checkout extends Component {
 
     }
 
-    handleAddressChnage = name => event => {
+    handleAddressChange = name => event => {
 
     }
 
     render() {
         const {classes} = this.props
         return (
+          <Card className={classes.card}>
+            <Typography type="title" className={classes.title}>
+                Checkout
+            </Typography>
+            <TextField id="name" label="Name" className={classes.textField} value={this.state.checkoutDetails.customer_name} onChange={this.handleCustomerChange('customer_name')} margin="normal"/><br/>
+            <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.checkoutDetails.customer_email} onChange={this.handleCustomerChange('customer_email')} margin="normal"/><br/>
+            <Typography type="subheading" component="h3" className={classes.subheading}>
+                Delivery Address
+            </Typography>
+            <TextField id="street" label="Street Address" className={classes.streetField} value={this.state.checkoutDetails.delivery_address.street} onChange={this.handleAddressChange('street')} margin="normal"/><br/>
+            <TextField id="city" label="City" className={classes.addressField} value={this.state.checkoutDetails.delivery_address.city} onChange={this.handleAddressChange('city')} margin="normal"/>
+            <TextField id="state" label="State" className={classes.addressField} value={this.state.checkoutDetails.delivery_address.state} onChange={this.handleAddressChange('state')} margin="normal"/><br/>
+            <TextField id="zipcode" label="Zip Code" className={classes.addressField} value={this.state.checkoutDetails.delivery_address.zipcode} onChange={this.handleAddressChange('zipcode')} margin="normal"/>
+            <TextField id="country" label="Country" className={classes.addressField} value={this.state.checkoutDetails.delivery_address.country} onChange={this.handleAddressChange('country')} margin="normal"/>
             <div>
-
+            <Elements>
+                <PlaceOrder checkoutDetails={this.state.checkoutDetails} />
+            </Elements>
             </div>
+          </Card>
         )
     }
 }
