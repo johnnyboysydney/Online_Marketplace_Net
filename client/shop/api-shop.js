556f858a52a1797b01d0239b19c08861b9ae03be
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 11:23:53 +1000

    adding api shop initial code

---
+++ client/shop/api-shop.js
@@ -0,0 +1,13 @@
+const create =() => {}
+
+const list = () => {}
+
+const listByOwner = () => {}
+
+const read = () => {}
+
+const update = () => {}
+
+const remove = () => {}
+
+export {create, list, listByOwner, read, update, remove}
