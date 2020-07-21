8395d33235c99b66d35e0cc840e6b6a812934972
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 19:11:24 +1000

    adding categories and initial code

--- client/product/Categories.js
+++ client/product/Categories.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
 import Card from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import Divider from 'material-ui/Divider'
+import GridList, { GridListTile } from 'material-ui/GridList'
+import Icon from 'material-ui/Icon'
+import {list} from './api-product.js'
+import Products from './Products'
 
 const styles = theme => ({
 
@@ -13,7 +19,19 @@ class Categories extends Component {
       products: [],
       selected: ''
     }
-    componentWillReceiveProps = (props) => {}
+    componentWillReceiveProps = (props) => {
+      this.setState({selected: props.categories[0]})
+      list({
+        category: props.categories[0]
+      })
+      .then((data) => {
+        if (data.error) {
+          console.log(data.error)
+        } else {
+          this.setState({products: data})
+        }
+      })
+    }
 
     listByCategory = category => event => {}
 
