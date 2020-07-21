f5549ac3743fff952f7272b3ed955c1a4fec6923
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 19:07:11 +1000

    added api-product

--- client/product/api-product.js
+++ client/product/api-product.js
@@ -1,14 +1,116 @@
 import queryString from 'query-string'
 
-const create = () => {}
-const read = () => {}
-const update = () => {}
-const remove = () => {}
-const listByShop = () => {}
-const listLatest = () => {}
-const listRelated = () => {}
-const listCategories = () => {}
-const list = () => {}
+const create = () => {
+  return fetch('/api/products/by/' + params.shopid, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer' + credentials.t
+    },
+    body: product
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => console.log(err))
+}
+
+const read = (params) => {
+  return fetch('/api/products/' + params.productId, {
+    method: 'GET'
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => console.log(err))
+}
+
+const update = (params, credentials, product) => {
+  return fetch('/api/product/' + params.shopId +'/'+params.productId, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    },
+    body: product
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+const remove = (params, credentials) => {
+  return fetch('/api/product/' + params.shopId +'/'+params.productId, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    }
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+const listByShop = (params) => {
+  return fetch('/api/products/by/'+params.shopId, {
+    method: 'GET'
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+const listLatest = () => {
+  return fetch('/api/products/latest', {
+    method: 'GET',
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch((err) => console.log(err))
+}
+
+const listRelated = (params) => {
+  return fetch('/api/products/related/'+params.productId, {
+    method: 'GET',
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch((err) => console.log(err))
+}
+
+const listCategories = () => {
+  return fetch('/api/products/categories', {
+    method: 'GET',
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch((err) => console.log(err))
+}
+
+const list = (params) => { 
+  const query = queryString.stringify(params)
+  return fetch('/api/products?'+query, {
+    method: 'GET',
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch((err) => console.log(err))
+}
 
 export {
     create,
@@ -20,4 +122,4 @@ export {
     listRelated,
     listCategories,
     list
-  }
+}
