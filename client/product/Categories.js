82c8972c40b3dce97ed221e84c038e551eb9456c
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 19:15:28 +1000

    adding react components

--- client/product/Categories.js
+++ client/product/Categories.js
@@ -33,15 +33,42 @@ class Categories extends Component {
       })
     }
 
-    listByCategory = category => event => {}
+    listByCategory = category => event => {
+      this.setState({selected: category})
+      list({
+        category: category
+      })
+      .then((data) => {
+        if (data.error) {
+          console.log(data.error)
+        } else {
+          this.setState({products: data})
+        }
+      })
+    }
 
     render() {
         const {classes} = this.props
         return (
-            <div>
-
-            </div>
-            )
+          <div>
+            <Card className={classes.card}>
+              <Typography type="title" className={classes.title}>
+                Explore by category
+              </Typography>
+              <div className={classes.root}>
+                <GridList className={classes.gridList} cols={4}>
+                  {this.props.categories.map((tile, i) => (
+                    <GridListTile key={i} className={classes.tileTitle} style={{height: '64px', backgroundColor: this.state.selected == tile? 'rgba(95, 139, 137, 0.56)':'rgba(95, 124, 139, 0.32)'}}>
+                      <span className={classes.link} onClick={this.listbyCategory(tile)}>{tile}  <Icon className={classes.icon}>{this.state.selected == tile && 'arrow_drop_down'}</Icon></span>
+                    </GridListTile>
+                  ))}
+                </GridList>
+                </div>
+                <Divider/>
+                <Products products={this.state.products} searched={false}/>
+              </Card>
+          </div>
+        )
     }
 }
 
