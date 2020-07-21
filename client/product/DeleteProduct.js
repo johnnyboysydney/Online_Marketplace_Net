ae704f712d500665cb658f478e8aadd34b78d8d8
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-21 19:23:09 +1000

    adding react components

--- client/product/DeleteProduct.js
+++ client/product/DeleteProduct.js
@@ -13,18 +13,52 @@ class DeleteProduct extends Component {
     clickButton = () => {
       this.setState({open: true})
     }
+
     DeleteProduct = () => {
-
+      const jwt = auth.isAuthenticated()
+      remove({
+        shopId: this.props.shopId,
+        productId: this.props.product._id
+      },
+      {t: jwt.token})
+      .then((data) => {
+        if (data.error) {
+          console.log(data.error)
+        } else {
+          this.setState({open: false}, () => {
+            this.props.onRemove(this.props.product)
+          })
+        }
+      })
     }
-    handleRequestClose = () => {
 
+    handleRequestClose = () => {
+      this.setState({open: false})
     }
 
     render() {
         return (
-            <div>
-
-            </div>
+          <span>
+            <IconButton aria-label="Delete" onClick={this.clickButton} color="secondary">
+              <DeleteIcon/>
+            </IconButton>
+            <Dialog open={this.state.open} onClose={this.handleRequestClose}>
+              <DialogTitle>{"Delete "+this.props.product.name}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Confirm to delete your product {this.props.product.name}.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleRequestClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={this.deleteProduct} color="secondary" autoFocus="autoFocus">
+                  Confirm
+                </Button>
+              </DialogActions>
+            </Dialog>
+        </span>
         )
     }
 }
