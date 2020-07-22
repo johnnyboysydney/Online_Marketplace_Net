7ce0c71f26a832bd8ad2d3576ca7f16c6666c109
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 09:00:06 +1000

    adding react layout and classes

--- client/product/NewProduct.js
+++ client/product/NewProduct.js
@@ -61,7 +61,28 @@ class NewProduct extends Component {
     const {classes} = this.props
     return (<div>
       <Card className={classes.card}>
-       
+        <CardContent>
+          <Typography type="headline" component="h2" className={classes.title}>
+            New Product
+          </Typography><br/>
+          <input accept="image/*" onChange={this.handleChange('image')} className={classes.input} id="icon-button-file" type="file"/>
+          <label htmlFor="icon-button-file">
+            <Button variant="raised" color="secondary" component="span">
+              Upload Photo
+              <FileUpload/>
+            </Button>
+          </label> <span className={classes.filename}>{this.state.image ? this.state.image.name : ''}</span><br/>
+          <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal"/><br/>
+          <TextField
+          /><br/>
+          <TextField id="category" label="Category" className={classes.textField} value={this.state.category} onChange={this.handleChange('category')} margin="normal"/><br/>
+          <TextField id="quantity" label="Quantity" className={classes.textField} value={this.state.quantity} onChange={this.handleChange('quantity')} type="number" margin="normal"/><br/>
+          <TextField id="price" label="Price" className={classes.textField} value={this.state.price} onChange={this.handleChange('price')} type="number" margin="normal"/><br/>
+        </CardContent>
+        <CardActions>
+          <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>Submit</Button>
+          <Link to={'/seller/shop/edit/'+this.match.params.shopId} className={classes.submit}><Button variant="raised">Cancel</Button></Link>
+        </CardActions>
       </Card>
     </div>)
   }
