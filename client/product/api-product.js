40d7837b0ebef14ca1e3f3229c13cd382a43df65
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 14:11:41 +1000

    refactoring code

--- client/product/api-product.js
+++ client/product/api-product.js
@@ -1,28 +1,24 @@
 import queryString from 'query-string'
-
-const create = () => {
-  return fetch('/api/products/by/' + params.shopid, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Bearer' + credentials.t
-    },
-    body: product
-  })
-  .then((response) => {
-    return response.json()
-  })
-  .catch((err) => console.log(err))
+const create = (params, credentials, product) => {
+  return fetch('/api/products/by/'+ params.shopId, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: product
+    })
+    .then((response) => {
+      return response.json()
+    }).catch((err) => console.log(err))
 }
 
 const read = (params) => {
   return fetch('/api/products/' + params.productId, {
     method: 'GET'
-  })
-  .then((response) => {
+  }).then((response) => {
     return response.json()
-  })
-  .catch((err) => console.log(err))
+  }).catch((err) => console.log(err))
 }
 
 const update = (params, credentials, product) => {
@@ -33,11 +29,9 @@ const update = (params, credentials, product) => {
       'Authorization': 'Bearer ' + credentials.t
     },
     body: product
-  })
-  .then((response) => {
+  }).then((response) => {
     return response.json()
-  })
-  .catch((err) => {
+  }).catch((err) => {
     console.log(err)
   })
 }
@@ -50,11 +44,9 @@ const remove = (params, credentials) => {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + credentials.t
     }
-  })
-  .then((response) => {
+  }).then((response) => {
     return response.json()
-  })
-  .catch((err) => {
+  }).catch((err) => {
     console.log(err)
   })
 }
@@ -62,11 +54,9 @@ const remove = (params, credentials) => {
 const listByShop = (params) => {
   return fetch('/api/products/by/'+params.shopId, {
     method: 'GET'
-  })
-  .then((response) => {
+  }).then((response) => {
     return response.json()
-  })
-  .catch((err) => {
+  }).catch((err) => {
     console.log(err)
   })
 }
@@ -74,52 +64,44 @@ const listByShop = (params) => {
 const listLatest = () => {
   return fetch('/api/products/latest', {
     method: 'GET',
-  })
-  .then(response => {
+  }).then(response => {
     return response.json()
-  })
-  .catch((err) => console.log(err))
+  }).catch((err) => console.log(err))
 }
 
 const listRelated = (params) => {
   return fetch('/api/products/related/'+params.productId, {
     method: 'GET',
-  })
-  .then(response => {
+  }).then(response => {
     return response.json()
-  })
-  .catch((err) => console.log(err))
+  }).catch((err) => console.log(err))
 }
 
 const listCategories = () => {
   return fetch('/api/products/categories', {
     method: 'GET',
-  })
-  .then(response => {
+  }).then(response => {
     return response.json()
-  })
-  .catch((err) => console.log(err))
+  }).catch((err) => console.log(err))
 }
 
-const list = (params) => { 
+const list = (params) => {
   const query = queryString.stringify(params)
   return fetch('/api/products?'+query, {
     method: 'GET',
-  })
-  .then(response => {
+  }).then(response => {
     return response.json()
-  })
-  .catch((err) => console.log(err))
+  }).catch((err) => console.log(err))
 }
 
 export {
-    create,
-    read,
-    update,
-    remove,
-    listByShop,
-    listLatest,
-    listRelated,
-    listCategories,
-    list
+  create,
+  read,
+  update,
+  remove,
+  listByShop,
+  listLatest,
+  listRelated,
+  listCategories,
+  list
 }
