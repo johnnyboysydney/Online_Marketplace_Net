973c97c99879ace950b3dcc71b5f422e69749cec
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 13:56:29 +1000

    refactroring code

--- client/cart/Cart.js
+++ client/cart/Cart.js
@@ -13,51 +13,49 @@ const styles = theme => ({
     margin: 30,
   }
 })
+
 class Cart extends Component {
-    state = {
-      checkout: false,
-      stripe: null
-    }
-    
-    componentDidMount = () => {
-      if (window.Stripe) {
+  state = {
+    checkout: false,
+    stripe: null
+  }
+
+  componentDidMount = () => {
+    if (window.Stripe) {
+      this.setState({stripe: window.Stripe(config.stripe_test_api_key)})
+    } else {
+      document.querySelector('#stripe-js').addEventListener('load', () => {
+        // Create Stripe instance once Stripe.js loads
         this.setState({stripe: window.Stripe(config.stripe_test_api_key)})
-      } else {
-        document.querySelector('#stripe-js').addEventListener('load', () => {
-          // Create Stripe instance once Stripe.js loads
-          this.setState({stripe: window.Stripe(config.stripe_test_api_key)})
-        })
-      }
+      })
     }
+  }
 
-    setCheckout = val => {
-      this.setState({ checkout: val })
-    }
+  setCheckout = val =>{
+    this.setState({checkout: val})
+  }
 
-    render() {
-      const {classes} = this.props
-      return (
-      <div className={classes.root}>
-        <Grid container spacing={24}>
-          <Grid item xs={6} sm={6}>
-            <CartItems checkout={this.state.checkout}
-                       setCheckout={this.setCheckout}/>
-          </Grid>
-          {this.state.checkout &&
+  render() {
+    const {classes} = this.props
+    return (<div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={6} sm={6}>
+          <CartItems checkout={this.state.checkout}
+                     setCheckout={this.setCheckout}/>
+        </Grid>
+        {this.state.checkout &&
           <Grid item xs={6} sm={6}>
             <StripeProvider stripe={this.state.stripe}>
               <Checkout/>
             </StripeProvider>
           </Grid>}
         </Grid>
-      </div>
-      )
-    }
+      </div>)
+  }
 }
 
 Cart.propTypes = {
-    classes: PropTypes.object.isRequired
-  }
-  
-  export default withStyles(styles)(Cart)
-  
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Cart)
