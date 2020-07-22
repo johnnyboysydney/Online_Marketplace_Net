c50b8380bc982c56b99ed2f872f86a868ba81c59
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 13:23:23 +1000

    adding MyOrders component and LoadOrders

--- client/order/MyOrders.js
+++ client/order/MyOrders.js
@@ -9,21 +9,48 @@ import Divider from 'material-ui/Divider'
 const styles = theme => ({})
 class MyOrders extends Component {
     state = {
-        orders: []
+        orders:[]
     }
-
+  
     loadOrders = () => {
-
+      const jwt = auth.isAuthenticated()
+      listByUser({
+        userId: jwt.user._id
+      }, {t: jwt.token}).then((data) => {
+        if (data.error) {
+          console.log(data.error)
+        } else {
+          this.setState({orders: data})
+        }
+      })
     }
-
+  
     componentDidMount = () => {
-        this.loadOrders()
+      this.loadOrders()
     }
-
+  
     render() {
-
+      const {classes} = this.props
+      return (
+        <Paper className={classes.root} elevation={4}>
+          <Typography type="title" className={classes.title}>
+            Your Orders
+          </Typography>
+          <List dense>
+            {this.state.orders.map((order, i) => {
+              return <span key={i}>
+                        <Link to={"/order/"+order._id}>
+                          <ListItem button>
+                            <ListItemText primary={<strong>{"Order # "+order._id}</strong>} secondary={(new Date(order.created)).toDateString()}/>
+                          </ListItem>
+                        </Link>
+                        <Divider/>
+                      </span>})}
+          </List>
+        </Paper>
+      )
     }
-}
+  }
 
 MyOrders.propTypes = {
     classes: PropTypes.object.isRequired
