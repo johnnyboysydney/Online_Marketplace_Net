5f1f3deed5e58cff1c637d0ae8fa80d520b9f772
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 13:53:03 +1000

    refactoring

--- client/user/DeleteUser.js
+++ client/user/DeleteUser.js
@@ -4,7 +4,7 @@ import IconButton from 'material-ui/IconButton'
 import Button from 'material-ui/Button'
 import DeleteIcon from 'material-ui-icons/Delete'
 import Dialog, {DialogActions, DialogContent, DialogContentText, DialogTitle} from 'material-ui/Dialog'
-import auth from '../auth/auth-helper'
+import auth from './../auth/auth-helper'
 import {remove} from './api-user.js'
 import {Redirect, Link} from 'react-router-dom'
 
@@ -13,18 +13,14 @@ class DeleteUser extends Component {
     redirect: false,
     open: false
   }
-  
   clickButton = () => {
     this.setState({open: true})
   }
-
   deleteAccount = () => {
     const jwt = auth.isAuthenticated()
     remove({
       userId: this.props.userId
-    }, 
-    {t: jwt.token})
-    .then((data) => {
+    }, {t: jwt.token}).then((data) => {
       if (data.error) {
         console.log(data.error)
       } else {
@@ -45,6 +41,7 @@ class DeleteUser extends Component {
       <IconButton aria-label="Delete" onClick={this.clickButton} color="secondary">
         <DeleteIcon/>
       </IconButton>
+
       <Dialog open={this.state.open} onClose={this.handleRequestClose}>
         <DialogTitle>{"Delete Account"}</DialogTitle>
         <DialogContent>
@@ -64,9 +61,7 @@ class DeleteUser extends Component {
     </span>)
   }
 }
-
 DeleteUser.propTypes = {
   userId: PropTypes.string.isRequired
 }
-
 export default DeleteUser
