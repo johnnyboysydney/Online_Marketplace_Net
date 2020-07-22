b273fec472ee41b507b2ebb6002b621031141fcb
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 12:24:28 +1000

    adding styles

--- client/product/Product.js
+++ client/product/Product.js
@@ -17,6 +17,45 @@ const styles = theme => ({
     },
     flex:{
       display:'flex'
+    },
+    card: {
+        padding:'24px 40px 40px'
+    },
+    subheading: {
+        margin: '24px',
+        color: theme.palette.openTitle
+    },
+    price: {
+        padding: '16px',
+        margin: '16px 0px',
+        display: 'flex',
+        backgroundColor: '#93c5ae3d',
+        fontSize: '1.3em',
+        color: '#375a53',
+    },
+    media: {
+        height: 200,
+        display: 'inline-block',
+        width: '50%',
+        marginLeft: '24px'
+    },
+    icon: {
+        verticalAlign: 'sub'
+    },
+    link:{
+        color: '#3e4c54b3',
+        fontSize: '0.9em'
+    },
+    addCart: {
+        width: '35px',
+        height: '35px',
+        padding: '10px 12px',
+        borderRadius: '0.25em',
+        backgroundColor: '#5f7c8b'
+    },
+    action: {
+        margin: '8px 24px',
+        display: 'inline-block'
     }
 })
 
