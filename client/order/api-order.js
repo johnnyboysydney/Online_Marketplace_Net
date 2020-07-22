fb50b126f147010326cc93f70fc45a5c784c6dff
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 10:47:06 +1000

    adding create api

--- client/order/api-order.js
+++ client/order/api-order.js
@@ -1,10 +1,31 @@
-const create = () => {}
+const create = () => {
+  return fetch('/api/orders/'+params.userId, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + credentials.t
+    },
+    body: JSON.stringify({order: order, token:token})
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => console.log(err))
+}
+
 const listByShop = () => {}
+
 const update = () => {}
+
 const cancelProduct = () => {}
+
 const processCharge = () => {}
+
 const getStatusValues = () => {}
+
 const listByUser = () => {}
+
 const read = () => {}
 
 export {
