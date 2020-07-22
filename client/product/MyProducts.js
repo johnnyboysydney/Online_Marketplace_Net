9a5a97a355577de959ace7f2b22f02a9fab1313a
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 08:43:58 +1000

    adding more items in the react layout

--- client/product/MyProducts.js
+++ client/product/MyProducts.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
 import Card, {CardMedia} from 'material-ui/Card'
+import { ListItem, Divider, IconButton } from 'material-ui'
+import Typography from 'material-ui/styles/typography'
+import { Edit, Delete } from 'material-ui-icons'
 
 const styles = theme => ({
 
@@ -48,6 +51,39 @@ class MyProducts extends Component {
             </Link>
           </span>
         </Typography>
+        <List>
+          {this.state.products.map((product) => {
+            return <span key = {i}>
+              <ListItem>
+                  <CardMedia
+                    className = { classes.cover }
+                    image = { '/api/product/image' + product._id + "?" + new Date().getTime() }
+                    title = { product.name }
+                  />
+                  <div>
+                    <Typography>
+                      { product.name }
+                    </Typography>
+                    <Typography>
+                      Quantity: { product.quantity } | Price: $ {product.price}
+                    </Typography>
+                  </div>
+                  <ListItemSecondaryAction>
+                      <Link>
+                        <IconButton>
+                          <Edit />
+                        </IconButton>
+                      </Link>
+                      <DeleteProduct
+                      product = { product }
+                      shopId = { this.props.shopId }
+                      onRemove = { this.removeProduct } />
+                  </ListItemSecondaryAction>
+              </ListItem>
+              <Divider />
+            </span>
+          })}
+        </List>
       </Card>)
   }
 }
