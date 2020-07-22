d304e5df0b3decce2258757766cf851bafe5ce60
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 10:32:57 +1000

    adding itemtotal and add item

--- client/cart/cart-helper.js
+++ client/cart/cart-helper.js
@@ -1,9 +1,26 @@
 const cart = {
     itemTotal(){
-
+        if (typeof window !== "undefined") {
+            if (localStorage.getItem('cart')) {
+              return JSON.parse(localStorage.getItem('cart')).length
+            }
+        }
+        return 0
     },
-    addItem(){
-
+    addItem(item, cb){
+        let cart = []
+        if (typeof window !== "undefined") {
+          if (localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'))
+          }
+          cart.push({
+            product: item,
+            quantity: 1,
+            shop: item.shop._id
+          })
+          localStorage.setItem('cart', JSON.stringify(cart))
+          cb()
+        }
     },
     updateCart(){
         
