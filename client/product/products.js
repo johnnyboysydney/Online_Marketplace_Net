c31a242fc3979e8b624a1e92760e9bf9b70e1c74
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 12:15:33 +1000

    renaming file

--- client/product/products.js
+++ client/product/products.js
@@ -7,7 +7,45 @@ import {Link} from 'react-router-dom'
 import AddToCart from '../cart/AddToCart'
 
 const styles = theme => ({
-
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        background: theme.palette.background.paper,
+        textAlign: 'left',
+        padding: '0 8px'
+    },
+    container: {
+        minWidth: '100%',
+        paddingBottom: '14px'
+    },
+    gridList: {
+        width: '100%',
+        minHeight: 200,
+        padding: '16px 0 10px'
+    },
+    title: {
+        padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+        color: theme.palette.openTitle,
+        width: '100%'
+    },
+    tile: {
+        textAlign: 'center'
+    },
+    image: {
+        height: '100%'
+    },
+    tileBar: {
+        backgroundColor: 'rgba(0, 0, 0, 0.72)',
+        textAlign: 'left'
+    },
+    tileTitle: {
+        fontSize:'1.1em',
+        marginBottom:'5px',
+        color:'rgb(189, 222, 219)',
+        display:'block'
+    }
 })
 
 class Products extends Component {
