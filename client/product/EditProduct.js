f228d908431de4e3da6ff3ec11d0bdd9a0e32123
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 08:04:58 +1000

    adding react components and clases

--- client/product/EditProduct.js
+++ client/product/EditProduct.js
@@ -7,52 +7,86 @@ import FileUpload from 'material-ui-icons/FileUpload'
 const styles = theme => ({})
 
 class EditProduct extends Component {
-    constructor({match}) {
-        super()
-        this.state = {
-          name: '',
-          description: '',
-          image: '',
-          category: '',
-          quantity: '',
-          price: '',
-          redirect: false,
-          error: ''
-        }
-        this.match = match
+  constructor({match}) {
+    super()
+    this.state = {
+      name: '',
+      description: '',
+      image: '',
+      category: '',
+      quantity: '',
+      price: '',
+      redirect: false,
+      error: ''
     }
+    this.match = match
+  }
 
-    componentDidMount = () => {
+  componentDidMount = () => {
+    this.productData = new FormData()
 
+  }
+  clickSubmit = () => {
+    const jwt = auth.isAuthenticated()
+
+  }
+  handleChange = name => event => {
+
+  }
+
+  render() {
+    const imageUrl = this.state.id
+          ? `/api/product/image/${this.state.id}?${new Date().getTime()}`
+          : '/api/product/defaultphoto'
+    if (this.state.redirect) {
+      return (<Redirect to={'/seller/shop/edit/'+this.match.params.shopId}/>)
     }
-
-    clickSubmit = () => {
-
-    }
-
-    handleChange = name => event => {
-
-    }
-
-    render() {
-        const imageUrl = this.state.id
-            ? `/api/product/image/${this.state.id}?${new Date().getTime()}`
-            : '/api/product/defaultphoto'
-        if (this.state.redirect) {
-            return (<Redirect to={'/seller/shop/edit/'+this.match.params.shopId}/>)
-        }
-        const {classes} = this.props
-        return (
-        <div>
-            <Card className={classes.card}>
-                <CardContent>
-                    <Typography type="headline" component="h2" className={classes.title}>
-                        Edit Product
-                    </Typography><br/>
-                </CardContent>
-            </Card>
-        </div>
-        )
-    }
-    
+    const {classes} = this.props
+    return (<div>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography type="headline" component="h2" className={classes.title}>
+            Edit Product
+          </Typography><br/>
+          <Avatar src={imageUrl} className={classes.bigAvatar}/><br/>
+          <input accept="image/*" onChange={this.handleChange('image')} className={classes.input} id="icon-button-file" type="file" />
+          <label htmlFor="icon-button-file">
+            <Button variant="raised" color="secondary" component="span">
+              Change Image
+              <FileUpload/>
+            </Button>
+          </label> <span className={classes.filename}>{this.state.image ? this.state.image.name : ''}</span><br/>
+          <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal"/><br/>
+          <TextField
+            id="multiline-flexible"
+            label="Description"
+            multiline
+            rows="3"
+            value={this.state.description}
+            onChange={this.handleChange('description')}
+            className={classes.textField}
+            margin="normal"
+          /><br/>
+          <TextField id="category" label="Category" className={classes.textField} value={this.state.category} onChange={this.handleChange('category')} margin="normal"/><br/>
+          <TextField id="quantity" label="Quantity" className={classes.textField} value={this.state.quantity} onChange={this.handleChange('quantity')} type="number" margin="normal"/><br/>
+          <TextField id="price" label="Price" className={classes.textField} value={this.state.price} onChange={this.handleChange('price')} type="number" margin="normal"/><br/>
+          {
+            this.state.error && (<Typography component="p" color="error">
+              <Icon color="error" className={classes.error}>error</Icon>
+              {this.state.error}</Typography>)
+          }
+        </CardContent>
+        <CardActions>
+          <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>Update</Button>
+          <Link to={'/seller/shops/edit/'+this.match.params.shopId} className={classes.submit}><Button variant="raised">Cancel</Button></Link>
+        </CardActions>
+      </Card>
+    </div>)
+  }
 }
+
+EditProduct.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(EditProduct)
