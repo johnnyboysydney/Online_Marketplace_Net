6db612fea7bbc78789021c54cb2fe5e4469f2891
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 12:32:30 +1000

    added react

--- client/shop/Shop.js
+++ client/shop/Shop.js
@@ -4,6 +4,10 @@ import Typography from 'material-ui/Typography'
 import Avatar from 'material-ui/Avatar'
 import Grid from 'material-ui/Grid'
 import PropTypes from 'prop-types'
+import {withStyles} from 'material-ui/styles'
+import {read} from './api-shop.js'
+import Products from '../product/products.js'
+import {listByShop} from './../product/api-product.js'
 
 const styles = theme => ({})
 
@@ -26,12 +30,35 @@ class Shop extends Component {
     }
 
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+        const logoUrl = this.state.shop._id
+              ? `/api/shops/logo/${this.state.shop._id}?${new Date().getTime()}`
+              : '/api/shops/defaultphoto'
+        const {classes} = this.props
+        return (<div className={classes.root}>
+          <Grid container spacing={24}>
+            <Grid item xs={4} sm={4}>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography type="headline" component="h2" className={classes.title}>
+                    {this.state.shop.name}
+                  </Typography>
+                  <br/>
+                  <Avatar src={logoUrl} className={classes.bigAvatar}/><br/>
+                    <Typography type="subheading" component="h2" className={classes.subheading}>
+                      {this.state.shop.description}
+                    </Typography><br/>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={8} sm={8}>
+              <Card>
+                <Typography type="title" component="h2" className={classes.productTitle}>Products</Typography>
+                <Products products={this.state.products} searched={false}/>
+              </Card>
+            </Grid>
+          </Grid>
+        </div>)
+      }
 }
 
 
