6142f7d183df2545c4d3f880bd64c20fd956b472
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-22 13:32:18 +1000

    adding style

--- client/order/ProductOrderEdit.js
+++ client/order/ProductOrderEdit.js
@@ -8,8 +8,34 @@ import TextField from 'material-ui/TextField'
 import Divider from 'material-ui/Divider'
 
 const styles = theme => ({
-
-})
+    nested: {
+      paddingLeft: theme.spacing.unit * 4,
+      paddingBottom: 0
+    },
+    listImg: {
+      width: '70px',
+      verticalAlign: 'top',
+      marginRight: '10px'
+    },
+    listDetails: {
+      display: "inline-block"
+    },
+    listQty: {
+      margin: 0,
+      fontSize: '0.9em',
+      color: '#5f7c8b'
+    },
+    textField: {
+      width: '160px',
+      marginRight: '16px'
+    },
+    statusMessage: {
+      position: 'absolute',
+      zIndex: '12',
+      right: '5px',
+      padding: '5px'
+    }
+  })
 
 class ProductOrderEdit extends Component {
     state = {
