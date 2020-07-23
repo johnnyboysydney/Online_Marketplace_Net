0e6431f9b9d2629a0b9c6097c25524940f287ec3
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:10:48 +1000

    adding clickbutton deleteshop handlerequestclose

--- client/shop/DeleteShop.js
+++ client/shop/DeleteShop.js
@@ -8,26 +8,37 @@ import auth from './../auth/auth-helper'
 import {remove} from './api-shop.js'
 
 class DeleteShop extends Component {
-    state = {
-      open: false
-    }
-    clickButton = () => {}
-
-    DeleteShop = () => {}
-
-    handleRequestClose = () => {}
-
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
-    }
-}
-
-DeleteShop.propTypes = {
-    shop: PropTypes.object.isRequired,
-    onRemove: PropTypes.func.isRequired
+  state = {
+    open: false
   }
-  export default DeleteShop
+  clickButton = () => {
+    this.setState({open: true})
+  }
+  deleteShop = () => {
+    const jwt = auth.isAuthenticated()
+    remove({
+      shopId: this.props.shop._id
+    }, {t: jwt.token}).then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        this.setState({open: false}, () => {
+          this.props.onRemove(this.props.shop)
+        })
+      }
+    })
+  }
+  handleRequestClose = () => {
+    this.setState({open: false})
+  }
+  render() {
+    return (<span>
+
+    </span>)
+  }
+}
+DeleteShop.propTypes = {
+  shop: PropTypes.object.isRequired,
+  onRemove: PropTypes.func.isRequired
+}
+export default DeleteShop
