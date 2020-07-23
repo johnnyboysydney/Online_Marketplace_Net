2369cd6b7b3007e89581cbd88a26e3488d456d70
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:15:37 +1000

    adding removeshop and loadshops

--- client/shop/MyShops.js
+++ client/shop/MyShops.js
@@ -3,14 +3,62 @@ import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
 import Paper from 'material-ui/Paper'
 import List, {ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText} from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+import IconButton from 'material-ui/IconButton'
+import Icon from 'material-ui/Icon'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
+import Edit from 'material-ui-icons/Edit'
+import Divider from 'material-ui/Divider'
+import auth from './../auth/auth-helper'
+import {listByOwner} from './api-shop.js'
+import {Redirect, Link} from 'react-router-dom'
+import DeleteShop from './DeleteShop'
 
-const styles = theme => ({})
+const styles = theme => ({
 
-class MyShops extends Component {}
+})
+class MyShops extends Component {
+  state = {
+      shops:[],
+      redirectToSignin: false
+  }
 
-MyShops.propTypes = {
-    classes: PropTypes.object.isRequired
+  loadShops = () => {
+    const jwt = auth.isAuthenticated()
+    listByOwner({
+      userId: jwt.user._id
+    }, {t: jwt.token}).then((data) => {
+      if (data.error) {
+        this.setState({redirectToSignin: true})
+      } else {
+        this.setState({shops: data})
+      }
+    })
+  }
+  removeShop = (shop) => {
+    const updatedShops = this.state.shops
+    const index = updatedShops.indexOf(shop)
+    updatedShops.splice(index, 1)
+    this.setState({shops: updatedShops})
+  }
+  componentDidMount = () => {
+    this.loadShops()
+  }
+  render() {
+    const {classes} = this.props
+    const redirectToSignin = this.state.redirectToSignin
+    if (redirectToSignin) {
+      return <Redirect to='/signin'/>
+    }
+    return (
+    <div>
+    
+    </div>)
+  }
 }
-  
+MyShops.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
 export default withStyles(styles)(MyShops)
-  
