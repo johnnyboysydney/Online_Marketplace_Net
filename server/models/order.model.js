ec6cbe92ff03aff3b9fc5ab947bebd6c638ba83c
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 20:45:09 +1000

    adding remaining model

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -1,18 +1,41 @@
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 
 const CartItemSchema = new mongoose.Schema({
-    product: {type: mongoose.Schema.ObjectId, red: 'Product'},
-    Quantity: Number,
-    shop: {type: mongoose.Schema.ObjectId, ref: 'Shop'},
-    status: {type:String,
-        default: 'Not processed',
-        enum: ['Not processed', 'Processing', 'Shipped', 'Delivered', 'Cancelled']}
+  product: {type: mongoose.Schema.ObjectId, ref: 'Product'},
+  quantity: Number,
+  shop: {type: mongoose.Schema.ObjectId, ref: 'Shop'},
+  status: {type: String,
+    default: 'Not processed',
+    enum: ['Not processed' , 'Processing', 'Shipped', 'Delivered', 'Cancelled']}
 })
-
-const CartItem = mongoose.model('CartITem', CartItemSchema)
-
+const CartItem = mongoose.model('CartItem', CartItemSchema)
 const OrderSchema = new mongoose.Schema({
-    products: [CartItemSchema]
+  products: [CartItemSchema],
+  customer_name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+  },
+  customer_email: {
+    type: String,
+    trim: true,
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email is required'
+  },
+  delivery_address: {
+    street: {type: String, required: 'Street is required'},
+    city: {type: String, required: 'City is required'},
+    state: {type: String},
+    zipcode: {type: String, required: 'Zip Code is required'},
+    country: {type: String, required: 'Country is required'}
+  },
+  payment_id: {},
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {type: mongoose.Schema.ObjectId, ref: 'User'}
 })
 
 const Order = mongoose.model('Order', OrderSchema)
