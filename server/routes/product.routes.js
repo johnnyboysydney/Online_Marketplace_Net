e20580e7e2942ed5e173ef1db279bd87e0d32881
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 20:53:53 +1000

    troubleshooting route issues

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -10,32 +10,30 @@ router.route('/api/products/by/:shopId')
   .get(productCtrl.listByShop)
 
 router.route('/api/products/latest')
-    .get(productCtrl.listLatest)
+  .get(productCtrl.listLatest)
 
 router.route('/api/products/related/:productId')
-    .get(productCtrl.listRelated)
-  
+  .get(productCtrl.listRelated)
+
 router.route('/api/products/categories')
-    .get(productCtrl.listCategories)
+  .get(productCtrl.listCategories)
 
 router.route('/api/products')
-    .get(productCtrl.list)
-  
+  .get(productCtrl.list)
+
 router.route('/api/products/:productId')
-    .get(productCtrl.read)
+  .get(productCtrl.read)
 
 router.route('/api/product/image/:productId')
-    .get(productCtrl.photo, productCtrl.defaultPhoto)
+  .get(productCtrl.photo, productCtrl.defaultPhoto)
+router.route('/api/product/defaultphoto')
+  .get(productCtrl.defaultPhoto)
 
-    router.route('/api/product/defaultphoto')
-    .get(productCtrl.defaultPhoto)
-  
 router.route('/api/product/:shopId/:productId')
-    .put(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.update)
-    .delete(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.remove)
-  
-router.param('shopId', shopCtrl.shopByID)
+  .put(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.update)
+  .delete(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.remove)
 
+router.param('shopId', shopCtrl.shopByID)
 router.param('productId', productCtrl.productByID)
 
 export default router
