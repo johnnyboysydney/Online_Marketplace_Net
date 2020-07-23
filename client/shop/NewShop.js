56de155ab44ac68bc135b9839862493096a9d33a
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:18:18 +1000

    adding styles

--- client/shop/NewShop.js
+++ client/shop/NewShop.js
@@ -12,7 +12,36 @@ import {create} from './api-shop.js'
 import {Link, Redirect} from 'react-router-dom'
 
 const styles = theme => ({
-
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    textAlign: 'center',
+    marginTop: theme.spacing.unit * 5,
+    paddingBottom: theme.spacing.unit * 2
+  },
+  error: {
+    verticalAlign: 'middle'
+  },
+  title: {
+    marginTop: theme.spacing.unit * 2,
+    color: theme.palette.openTitle,
+    fontSize: '1em'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 300
+  },
+  submit: {
+    margin: 'auto',
+    marginBottom: theme.spacing.unit * 2
+  },
+  input: {
+    display: 'none'
+  },
+  filename:{
+    marginLeft:'10px'
+  }
 })
 
 class NewShop extends Component {
