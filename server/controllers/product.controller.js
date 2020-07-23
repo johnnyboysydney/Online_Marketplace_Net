19b1a6d5262ec4de86c119947b84225cb9526939
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:34:11 +1000

    adding defaultphoto, update, photo

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -31,7 +31,7 @@ const create = (req, res, next) => {
     })
 }
 
-const productByID = () => {
+const productByID = (req, res, next, id) => {
   Product.findById(id).populate('shop', '_id name').exec((err, product) => {
     if (err || !product)
       return res.status('400').json({
@@ -42,12 +42,16 @@ const productByID = () => {
   })
 }
 
-const photo = () => {
-
+const photo = (req, res, next) => {
+  if(req.product.image.data){
+    res.set("Content-Type", req.product.image.contentType)
+    return res.send(req.product.image.data)
+  }
+  next()
 }
 
-const defaultPhoto = () => {
-
+const defaultPhoto = (req, res) => {
+  return res.sendFile(process.cwd()+profileImage)
 }
 
 const read = (req, res) => {
@@ -55,10 +59,32 @@ const read = (req, res) => {
     return res.json(req.product)
 }
 
-const update = () => {
-
+const update = (req, res, next) => {
+  let form = new formidable.IncomingForm()
+  form.keepExtensions = true
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        message: "Photo could not be uploaded"
+      })
+    }
+    let product = req.product
+    product = _.extend(product, fields)
+    product.updated = Date.now()
+    if(files.image){
+      product.image.data = fs.readFileSync(files.image.path)
+      product.image.contentType = files.image.type
+    }
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).send({
+          error: errorHandler.getErrorMessage(err)
+        })
+      }
+      res.json(result)
+    })
+  })
 }
-
 const remove = () => {
 
 }
