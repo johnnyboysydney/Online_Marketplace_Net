ed1fac083fceaa5e12d65c555e004f94d4b0f674
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 20:34:59 +1000

    adding routes for products

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,8 +1,22 @@
 import express from 'express'
-//import productCtrl from '../controllers/product.controller'
-//import authCtrl from '../controllers/auth.controller'
-//import shopCtrl from '../controllers/shop.controller'
+import productCtrl from '../controllers/product.controller'
+import authCtrl from '../controllers/auth.controller'
+import shopCtrl from '../controllers/shop.controller'
 
 const router = express.Router()
+
+router.route('/api/products/by/:shopId')
+  .post(authCtrl.requireSignin, shopCtrl.isOwner, productCtrl.create)
+  .get(productCtrl.listByShop)
+
+
+router.route('/api/products/latest')
+    .get(productCtrl.listLatest)
+
+router.route('/api/products/related/:productId')
+    .get(productCtrl.listRelated)
+  
+router.route('/api/products/categories')
+    .get(productCtrl.listCategories)
 
 export default router
