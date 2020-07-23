be7ab444b17b205863bf771bc76c5db38f6e5cc4
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:22:51 +1000

    adding reqact components

--- client/shop/Shops.js
+++ client/shop/Shops.js
@@ -58,7 +58,29 @@ class Shops extends Component {
     return (
     <div>
       <Paper className={classes.root} elevation={4}>
-
+        <Typography type="title" className={classes.title}>
+          All Shops
+        </Typography>
+        <List dense>
+          {this.state.shops.map((shop, i) => {
+            return <Link to={"/shops/"+shop._id} key={i}>
+              <Divider/>
+              <ListItem button>
+                <ListItemAvatar>
+                  <Avatar className={classes.avatar}  src={'/api/shops/logo/'+shop._id+"?" + new Date().getTime()}/>
+                </ListItemAvatar>
+                <div className={classes.details}>
+                  <Typography type="headline" component="h2" color="primary" className={classes.shopTitle}>
+                    {shop.name}
+                  </Typography>
+                  <Typography type="subheading" component="h4" className={classes.subheading}>
+                    {shop.description}
+                  </Typography>
+                </div>
+              </ListItem>
+              <Divider/>
+            </Link>})}
+        </List>
       </Paper>
     </div>)
   }
