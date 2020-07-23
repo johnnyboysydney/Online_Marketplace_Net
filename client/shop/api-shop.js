6a3a9825e6ca8d31ada7e7c99f8a48d022d40bf7
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:07:29 +1000

    added list function

--- client/shop/api-shop.js
+++ client/shop/api-shop.js
@@ -1,26 +1,46 @@
-const create =(params, credentials, shop) => {
-    return fetch('/api/shops/by/'+ params.userId, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Bearer ' + credentials.t
-        },
-        body: shop
-      })
-      .then((response) => {
-        return response.json()
-      })
-      .catch((err) => console.log(err))
+const create = (params, credentials, shop) => {
+  return fetch('/api/shops/by/'+ params.userId, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: shop
+    })
+    .then((response) => {
+      return response.json()
+    }).catch((err) => console.log(err))
 }
 
-const list = () => {}
+const list = () => {
+  return fetch('/api/shops', {
+    method: 'GET',
+  }).then(response => {
+    return response.json()
+  }).catch((err) => console.log(err))
+}
 
-const listByOwner = () => {}
+const listByOwner = (params, credentials) => {
 
-const read = () => {}
+}
 
-const update = () => {}
+const read = (params, credentials) => {
 
-const remove = () => {}
+}
 
-export {create, list, listByOwner, read, update, remove}
+const update = (params, credentials, shop) => {
+
+}
+
+const remove = (params, credentials) => {
+
+}
+
+export {
+  create,
+  list,
+  listByOwner,
+  read,
+  update,
+  remove
+}
