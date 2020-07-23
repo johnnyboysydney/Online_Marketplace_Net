d7f03a26dac8360745c8a39070a605817b49d580
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:21:59 +1000

    adding componentdidmount and loadshops as well as missing imports

--- client/shop/Shops.js
+++ client/shop/Shops.js
@@ -2,28 +2,43 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
 import Paper from 'material-ui/Paper'
+import List, {ListItem, ListItemAvatar} from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+import Typography from 'material-ui/Typography'
+import Divider from 'material-ui/Divider'
+import {list} from './api-shop.js'
+import {Link} from 'react-router-dom'
+const styles = theme => ({
 
-const styles = theme => ({})
+})
 class Shops extends Component {
-    state = {
-        shops:[]
-    }
-    loadShops = () => {}
-    componentDidMount = () => {
+  state = {
+      shops:[]
+  }
+  loadShops = () => {
+    list().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        this.setState({shops: data})
+      }
+    })
+  }
+  componentDidMount = () => {
+    this.loadShops()
+  }
+  render() {
+    const {classes} = this.props
+    return (
+    <div>
+      <Paper className={classes.root} elevation={4}>
 
-    }
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
-    }
+      </Paper>
+    </div>)
+  }
+}
+Shops.propTypes = {
+  classes: PropTypes.object.isRequired
 }
 
-Shops.propTypes = {
-    classes: PropTypes.object.isRequired
-  }
-  
-  export default withStyles(styles)(Shops)
-  
+export default withStyles(styles)(Shops)
