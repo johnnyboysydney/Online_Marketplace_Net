045728d9e698cf73e5957034ec38b719b05daf84
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-23 19:35:09 +1000

    adding remove, listbyshop

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -85,12 +85,28 @@ const update = (req, res, next) => {
     })
   })
 }
-const remove = () => {
 
+const remove = (req, res, next) => {
+  let product = req.product
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+    res.json(deletedProduct)
+  })
 }
 
-const listByShop = () => {
-
+const listByShop = (req, res) => {
+  Product.find({shop: req.shop._id}, (err, products) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+    res.json(products)
+  }).populate('shop', '_id name').select('-image')
 }
 
 const listLatest = () => {
