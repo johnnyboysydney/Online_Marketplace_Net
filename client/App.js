2f4478332adfb06b400aee444d1be4efd7d59a82
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-26 18:26:26 +1000

    refactoring

--- client/App.js
+++ client/App.js
@@ -1,37 +1,23 @@
 import React from 'react'
 import MainRouter from './MainRouter'
 import {BrowserRouter} from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './theme'
 import { hot } from 'react-hot-loader'
-import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles'
-import {indigo, pink} from 'material-ui/colors'
 
-// Create a theme instance.
-const theme = createMuiTheme({ 
-  palette: {
-    primary:{
-      light: '#0979C9',
-      main: '#2874A6',
-      dark: '#0923C9',
-      contrastText: ''
-    },
-    secondary: {
-      light: '#EE96D7',
-      main: '#8F09C9',
-      dark: '#4809C9',
-      contrastText: '#000',
-    },
-    openTitle: indigo['400'],
-    protectedTitle: pink['400'],
-    type: 'light'
-  }
-})
-
-const App = () => (
+const App = () => {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }, [])
+  return (
   <BrowserRouter>
-    <MuiThemeProvider theme={theme}>
-      <MainRouter/>
-    </MuiThemeProvider>
+      <ThemeProvider theme={theme}>
+        <MainRouter/>
+      </ThemeProvider>
   </BrowserRouter>
-)
+)}
 
 export default hot(module)(App)
