b239206bf2c39d4928ed0d1f6a26279fea9d2824
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-26 18:26:01 +1000

    adding uaction sections commented

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+// Imports and dependencies
+import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './core/Home'
 import Users from './user/Users'
@@ -21,17 +22,15 @@ import StripeConnect from './user/StripeConnect'
 import ShopOrders from './order/ShopOrders'
 import Order from './order/Order'
 
-class MainRouter extends Component {
-  // Removes the server-side injected CSS when React component mounts
-  componentDidMount() {
-    const jssStyles = document.getElementById('jss-server-side')
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles)
-    }
-  }
+// Adding future auction imports and dependencies
+// import MyAuctions from './auction/MyAuctions'
+// import OpenAuctions from './auction/OpenAuctions'
+// import NewAuction from './auction/NewAuction'
+// import EditAuction from './auction/EditAuction'
+// import Auction from './auction/Auction'
 
-  render() {
-    return (<div>
+const MainRouter = () => {
+  return (<div>
       <Menu/>
       <Switch>
         <Route exact path="/" component={Home}/>
@@ -54,11 +53,15 @@ class MainRouter extends Component {
         <PrivateRoute path="/seller/shop/edit/:shopId" component={EditShop}/>
         <PrivateRoute path="/seller/:shopId/products/new" component={NewProduct}/>
         <PrivateRoute path="/seller/:shopId/:productId/edit" component={EditProduct}/>
-        
+
         <Route path="/seller/stripe/connect" component={StripeConnect}/>
+        {/* <PrivateRoute path="/myauctions" component={MyAuctions}/>
+        <PrivateRoute path="/auction/new" component={NewAuction}/>
+        <PrivateRoute path="/auction/edit/:auctionId" component={EditAuction}/>
+        <Route path="/auction/:auctionId" component={Auction}/>
+        <Route path="/auctions/all" component={OpenAuctions}/> */}
       </Switch>
     </div>)
-  }
 }
 
 export default MainRouter
