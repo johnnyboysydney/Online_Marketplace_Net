5c112dd9ebf48aa549d3c7440c8d21aeb3fed598
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 16:18:47 +1000

    adding the mount auctionroutes and importing auction routes

--- server/express.js
+++ server/express.js
@@ -10,6 +10,7 @@ import authRoutes from './routes/auth.routes'
 import shopRoutes from './routes/shop.routes'
 import productRoutes from './routes/product.routes'
 import orderRoutes from './routes/order.routes'
+import auctionRoutes from './routes/auction.routes'
 
 // modules for server side rendering
 import React from 'react'
@@ -48,6 +49,7 @@ app.use('/', authRoutes)
 app.use('/', shopRoutes)
 app.use('/', productRoutes)
 app.use('/', orderRoutes)
+app.use('/', auctionRoutes)
 
 app.get('*', (req, res) => {
   const sheets = new ServerStyleSheets()
