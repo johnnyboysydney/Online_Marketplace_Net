408e2b8ba1b3f04832e9e5bdd569d23925d39709
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 16:02:06 +1000

    adding auction controller

--- server/controllers/auction.controller.js
+++ server/controllers/auction.controller.js
@@ -2,3 +2,40 @@
 // create, photo, defaultphoto, read, update, remove, listbyseller
 // And will create new ones for the auction:
 // auctionbyID, listbybidder.
+import Auction from '../models/auction.model'
+import extend from 'lodash/extend'
+import errorHandler from '../helpers/dbErrorHandler'
+import formidable from 'formidable'
+import fs from 'fs'
+import defaultImage from './../../client/assets/images/default.png'
+
+
+
+const create = () => {}
+const auctionByID  = () => {}
+const photo = () => {}
+const defaultPhoto = () => {}
+const read = () => {}
+const update = () => {}
+const remove = () => {}
+const listOpen = () => {}
+const listBySeller = () => {}
+const listByBidder = () => {}
+const isSeller = () => {}
+
+export default {
+  create,
+  auctionByID,
+  photo,
+  defaultPhoto,
+  listOpen,
+  listBySeller,
+  listByBidder,
+  read,
+  update,
+  isSeller,
+  remove
+}
+
+
+
