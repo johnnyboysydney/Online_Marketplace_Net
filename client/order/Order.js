92afa4f34e4bb5f95545cecb09f98da0774228f6
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 15:45:49 +1000

    troubleshooting order

--- client/order/Order.js
+++ client/order/Order.js
@@ -1,115 +1,43 @@
-import React, {useState, useEffect}  from 'react'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
+import React, {useState, useEffect} from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 import Typography from '@material-ui/core/Typography'
-import Grid from '@material-ui/core/Grid'
 import Divider from '@material-ui/core/Divider'
-import {makeStyles} from '@material-ui/core/styles'
-import {read} from './api-order.js'
+import auth from './../auth/auth-helper'
+import {listByUser} from './api-order.js'
 import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
-  card: {
-    textAlign: 'center',
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(2),
-    flexGrow: 1,
-    margin: 30,
-  },
-  cart: {
-    textAlign: 'left',
-    width: '100%',
-    display: 'inline-flex'
-  },
-  details: {
-    display: 'inline-block',
-    width: "100%",
-    padding: "4px"
-  },
-  content: {
-    flex: '1 0 auto',
-    padding: '16px 8px 0px'
-  },
-  cover: {
-    width: 160,
-    height: 125,
-    margin: '8px'
-  },
-  info: {
-    color: 'rgba(83, 170, 146, 0.82)',
-    fontSize: '0.95rem',
-    display: 'inline'
-  },
-  thanks:{
-    color: 'rgb(136, 183, 107)',
-    fontSize: '0.9rem',
-    fontStyle: 'italic'
-  },
-  innerCardItems: {
-    textAlign: 'left',
-    margin: '24px 10px 24px 24px',
-    padding: '24px 20px 40px 20px',
-    backgroundColor: '#80808017'
-  },
-  innerCard: {
-    textAlign: 'left',
-    margin: '24px 24px 24px 10px',
-    padding: '30px 45px 40px 45px',
-    backgroundColor: '#80808017'
-  },
+  root: theme.mixins.gutters({
+    maxWidth: 600,
+    margin: '12px 24px',
+    padding: theme.spacing(3),
+    backgroundColor: '#3f3f3f0d'
+  }),
   title: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(1),
-    color: theme.palette.protectedTitle,
-    fontSize: '1.2em'
-  },
-  subheading: {
-    marginTop: theme.spacing(1),
+    margin: `${theme.spacing(2)}px 0 12px ${theme.spacing(1)}px`,
     color: theme.palette.openTitle
-  },
-  productTitle: {
-    fontSize: '1.15em',
-    marginBottom: '5px'
-  },
-  itemTotal: {
-    float: 'right',
-    marginRight: '40px',
-    fontSize: '1.5em',
-    color: 'rgb(72, 175, 148)'
-  },
-  itemShop: {
-    display: 'block',
-    fontSize: '1em',
-    color: '#78948f'
-  },
-  checkout: {
-    float: 'right',
-    margin: '24px'
-  },
-  total: {
-    fontSize: '1.2em',
-    color: 'rgb(53, 97, 85)',
-    marginRight: '16px',
-    fontWeight: '600',
-    verticalAlign: 'bottom'
   }
 }))
 
-export default function Order({match}) {
+export default function MyOrders(){
   const classes = useStyles()
-  const [order, setOrder] = useState({products:[], delivery_address:{}})
+  const [orders, setOrders] = useState([])
+  const jwt = auth.isAuthenticated()
 
   useEffect(() => {
     const abortController = new AbortController()
     const signal = abortController.signal
-    read({
-      orderId: match.params.orderId
-    }).then((data) => {
+    listByUser({
+      userId: jwt.user._id
+    }, {t: jwt.token}).then((data) => {
       if (data.error) {
         console.log(data.error)
       } else {
-        setOrder(data)
+        setOrders(data)
       }
     })
     return function cleanup(){
@@ -117,67 +45,22 @@ export default function Order({match}) {
     }
   }, [])
 
-  const getTotal = () => {
-    return order.products.reduce((a, b) => {
-       const quantity = b.status == "Cancelled" ? 0 : b.quantity
-        return a + (quantity*b.product.price)
-    }, 0)
-  }
-
     return (
-      <Card className={classes.card}>
-        <Typography type="headline" component="h2" className={classes.title}>
-            Order Details
+      <Paper className={classes.root} elevation={4}>
+        <Typography type="title" className={classes.title}>
+          Your Orders
         </Typography>
-        <Typography type="subheading" component="h2" className={classes.subheading}>
-            Order Code: <strong>{order._id}</strong> <br/> Placed on {(new Date(order.created)).toDateString()}
-        </Typography><br/>
-        <Grid container spacing={4}>
-            <Grid item xs={7} sm={7}>
-                <Card className={classes.innerCardItems}>
-                  {order.products.map((item, i) => {
-                    return  <span key={i}>
-                      <Card className={classes.cart} >
-                        <CardMedia
-                          className={classes.cover}
-                          image={'/api/product/image/'+item.product._id}
-                          title={item.product.name}
-                        />
-                        <div className={classes.details}>
-                          <CardContent className={classes.content}>
-                            <Link to={'/product/'+item.product._id}><Typography type="title" component="h3" className={classes.productTitle} color="primary">{item.product.name}</Typography></Link>
-                            <Typography type="subheading" component="h3" className={classes.itemShop} color="primary">$ {item.product.price} x {item.quantity}</Typography>
-                            <span className={classes.itemTotal}>${item.product.price * item.quantity}</span>
-                            <span className={classes.itemShop}>Shop: {item.shop.name}</span>
-                            <Typography type="subheading" component="h3" color={item.status == "Cancelled" ? "error":"secondary"}>Status: {item.status}</Typography>
-                          </CardContent>
-                        </div>
-                      </Card>
+        <List dense>
+          {orders.map((order, i) => {
+            return <span key={i}>
+                      <Link to={"/order/"+order._id}>
+                        <ListItem button>
+                          <ListItemText primary={<strong>{"Order # "+order._id}</strong>} secondary={(new Date(order.created)).toDateString()}/>
+                        </ListItem>
+                      </Link>
                       <Divider/>
-                    </span>})
-                  }
-                  <div className={classes.checkout}>
-                    <span className={classes.total}>Total: ${getTotal()}</span>
-                  </div>
-                </Card>
-            </Grid>
-            <Grid item xs={5} sm={5}>
-              <Card className={classes.innerCard}>
-                <Typography type="subheading" component="h2" className={classes.productTitle} color="primary">
-                 Deliver to:
-                </Typography>
-                <Typography type="subheading" component="h3" className={classes.info} color="primary"><strong>{order.customer_name}</strong></Typography><br/>
-                <Typography type="subheading" component="h3" className={classes.info} color="primary">{order.customer_email}</Typography><br/>
-                <br/>
-                <Divider/>
-                <br/>
-                <Typography type="subheading" component="h3" className={classes.itemShop} color="primary">{order.delivery_address.street}</Typography>
-                <Typography type="subheading" component="h3" className={classes.itemShop} color="primary">{order.delivery_address.city}, {order.delivery_address.state} {order.delivery_address.zipcode}</Typography>
-                <Typography type="subheading" component="h3" className={classes.itemShop} color="primary">{order.delivery_address.country}</Typography><br/>
-                <Typography type="subheading" component="h3" className={classes.thanks} color="primary">Thank you for shopping with us! <br/>You can track the status of your purchased items on this page.</Typography>
-              </Card>
-            </Grid>
-        </Grid>
-      </Card>
+                    </span>})}
+        </List>
+      </Paper>
     )
 }
