33e0099d32b06536650e373b6b5e4c5cda59d472
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 17:33:09 +1000

    adding calculatetime and imports

---
+++ client/auction/Auctions.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+
+const calculateTimeLeft = (date) => {
+  const difference = date - new Date()
+  let timeLeft = {}
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+      timeEnd: false
+    }
+  } else {
+      timeLeft = {timeEnd: true}
+  }
+  return timeLeft
+}
+
+export default function Auctions(props){}
