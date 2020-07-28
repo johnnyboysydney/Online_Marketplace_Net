52f9f1db0e8d9f589a089938f0d2559c9d78c48d
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 17:39:55 +1000

    adding remaining imports and react components

--- client/auction/DeleteAuction.js
+++ client/auction/DeleteAuction.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types'
 import IconButton from '@material-ui/core/IconButton'
 import Button from '@material-ui/core/Button'
 import DeleteIcon from '@material-ui/icons/Delete'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import auth from '../auth/auth-helper'
+import {remove} from './api-auction.js'
 
 export default function DeleteAuction(props) {
   const [open, setOpen] = useState(false)
@@ -23,4 +30,33 @@ export default function DeleteAuction(props) {
       }
     })
   }
+  const handleRequestClose = () => {
+    setOpen(false)
+  }
+    return (<span>
+      <IconButton aria-label="Delete" onClick={clickButton} color="secondary">
+        <DeleteIcon/>
+      </IconButton>
+
+      <Dialog open={open} onClose={handleRequestClose}>
+        <DialogTitle>{"Delete "+props.auction.itemName}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Confirm to delete your auction {props.auction.itemName}.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleRequestClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={deleteAuction} color="secondary" autoFocus="autoFocus">
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </span>)
+}
+DeleteAuction.propTypes = {
+  auction: PropTypes.object.isRequired,
+  onRemove: PropTypes.func.isRequired
 }
