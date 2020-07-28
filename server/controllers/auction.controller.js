a47f50b200f793920d4425ceeda51f0ca22811f2
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 16:28:32 +1000

    adding create function

--- server/controllers/auction.controller.js
+++ server/controllers/auction.controller.js
@@ -11,7 +11,31 @@ import defaultImage from './../../client/assets/images/default.png'
 
 
 
-const create = () => {}
+const create = (req, res) => {
+  let form = new formidable.IncomingForm()
+  form.keepExtensions = true
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      res.status(400).json({
+        message: "Image could not be uploaded"
+      })
+    }
+    let auction = new Auction(fields)
+    auction.seller= req.profile
+    if(files.image){
+      auction.image.data = fs.readFileSync(files.image.path)
+      auction.image.contentType = files.image.type
+    }
+    try {
+      let result = await auction.save()
+      res.status(200).json(result)
+    }catch (err){
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+  })
+}
 const auctionByID  = () => {}
 const photo = () => {}
 const defaultPhoto = () => {}
