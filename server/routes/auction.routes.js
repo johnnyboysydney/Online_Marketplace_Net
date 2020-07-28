2a76a861c00f66de16c7d2361e5704b8e5d6a774
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 16:13:37 +1000

    adding auction routes

---
+++ server/routes/auction.routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import userCtrl from '../controllers/user.controller'
+import authCtrl from '../controllers/auth.controller'
+import auctionCtrl from '../controllers/auction.controller'
+
+const router = express.Router()
+
+export default router
