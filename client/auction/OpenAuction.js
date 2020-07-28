14854a64eda86d1aba4fc1dd6e58bb63b1fbc076
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 17:48:10 +1000

    adding react components

--- client/auction/OpenAuction.js
+++ client/auction/OpenAuction.js
@@ -9,4 +9,38 @@ const useStyles = makeStyles(theme => ({
 
 }))
 
-export default function OpenAuctions(){}
+export default function OpenAuctions(){
+  const classes = useStyles()
+  const [auctions, setAuctions] = useState([])
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    const signal = abortController.signal
+    listOpen(signal).then((data) => {
+      if (data.error) {
+        setRedirectToSignin(true)
+      } else {
+        setAuctions(data)
+      }
+    })
+    return function cleanup(){
+      abortController.abort()
+    }
+  }, [])
+
+  const removeAuction = (auction) => {
+    const updatedAuctions = [...auctions]
+    const index = updatedAuctions.indexOf(auction)
+    updatedAuctions.splice(index, 1)
+    setAuctions(updatedAuctions)
+  }
+    return (
+    <div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography type="title" className={classes.title}>
+          All Auctions
+        </Typography>
+        <Auctions auctions={auctions} removeAuction={removeAuction}/>
+      </Paper>
+    </div>)
+}
