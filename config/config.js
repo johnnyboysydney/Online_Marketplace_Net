505eb4e2ef96631cd97e4a4ea69d2f29dd9b4df6
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 21:24:31 +1000

    adatping to .env

--- config/config.js
+++ config/config.js
@@ -1,15 +1,19 @@
+import dotenv from 'dotenv'
+
+require('dotenv').config()
+
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "51H6E6yEV0d7rwUXNpphZoFt42839",
+  jwtSecret: process.env.JWT_SECRET,
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/mernproject',
-    stripe_connect_test_client_id: 'ca_HiS0upcpmPZ8oY88WWCIstQ1BnrZtBXE',
-    stripe_test_secret_key: 'sk_test_51H6E6yEV0d7rwUXNpphZoFt42839vrqg8xgsdAnQcAuSrlxUS1GPRMrwP0gvdaqhgS9PpeESEwZtEorIo5cvaG95003wMGphVr',
-    stripe_test_api_key: 'pk_test_51H6E6yEV0d7rwUXNgJU9onIdFyA4BksuQr7yFpFPAUryF7mVd0naFCx1nggH7D7OAZXgCywmBSooD1YOmTZCf1Ir00m1HwJ988'
+    stripe_connect_test_client_id: process.env.STRIPE_CLIENT_ID,
+    stripe_test_secret_key: process.env.STRIPE_SECRET_KEY,
+    stripe_test_api_key: process.env.STRIPE_API_KEY
 }
 
 export default config
