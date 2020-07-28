9a6f3734591f12f8dfdf4c9fe8e85335539e4a46
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 16:14:02 +1000

    adding more routes

--- server/routes/auction.routes.js
+++ server/routes/auction.routes.js
@@ -5,4 +5,17 @@ import auctionCtrl from '../controllers/auction.controller'
 
 const router = express.Router()
 
+router.route('/api/auctions')
+  .get(auctionCtrl.listOpen)
+
+router.route('/api/auctions/bid/:userId')
+  .get(auctionCtrl.listByBidder)
+
+router.route('/api/auction/:auctionId')
+  .get(auctionCtrl.read)
+
+router.route('/api/auctions/by/:userId')
+  .post(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.isSeller, auctionCtrl.create)
+  .get(authCtrl.requireSignin, authCtrl.hasAuthorization, auctionCtrl.listBySeller)
+  
 export default router
