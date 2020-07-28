a2f4eefdaea6910125fda13672efe8e639e32d31
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-28 17:28:15 +1000

    adding OpenAuction

---
+++ client/auction/OpenAuction.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import {listOpen} from './api-auction.js'
+import Auctions from './Auctions'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function OpenAuctions(){}
